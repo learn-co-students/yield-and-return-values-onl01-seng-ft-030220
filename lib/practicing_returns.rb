b15fb58a87cda5646c7ee6240cd12a66eec92496
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 15:40:51 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def hello(array)
   i = 0
+  new_ary = []
   while i < array.length
-    yield(array[i])
+    new_ary << ield(array[i])
     i += 1
   end
+  new_ary
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
